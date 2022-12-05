4a1936bd8aab9861593e605e2e0c382f052f9dd1
Tobias Fuchs <tobias.fuchst@tfox.tech>
2022-12-05 19:03:02 +0100

    add autoopen minimap

--- config.lua
+++ config.lua
@@ -248,6 +248,34 @@ lvim.plugins = {
 --   end,
 -- })
 
+lvim.autocommands = {
+  {
+    { "BufEnter", "Filetype" },
+    {
+      desc = "Open mini.map and exclude some filetypes",
+      pattern = { "*" },
+      callback = function()
+        local exclude_ft = {
+          "qf",
+          "NvimTree",
+          "toggleterm",
+          "TelescopePrompt",
+          "alpha",
+          "netrw",
+        }
+
+        local map = require('mini.map')
+        if vim.tbl_contains(exclude_ft, vim.o.filetype) then
+          vim.b.minimap_disable = true
+          map.close()
+        elseif vim.o.buftype == "" then
+          map.open()
+        end
+      end,
+    },
+  },
+}
+
 vim.o.number = true
 vim.o.relativenumber = true
 vim.o.wrap = true
